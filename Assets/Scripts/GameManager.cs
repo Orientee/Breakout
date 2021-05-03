29324ed5815481fbf665d7a4c81aab725606ffd6
Pawel <71929605+Orientee@users.noreply.github.com>
2021-05-03 13:55:26 +0200

    Assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public bool IsGameStarted { get; set; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
