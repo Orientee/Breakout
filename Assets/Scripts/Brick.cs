82d98ab69f3129a352f068970280e832e0b7bc46
Pawel <71929605+Orientee@users.noreply.github.com>
2021-05-04 14:36:52 +0200

    Brick and particles

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class Brick : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    public int HitPoints = 1;
+    public ParticleSystem DestroyEffect;
+
+    public static event Action<Brick> OnBrickDestruction;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = BricksManager.Instance.Sprites[this.HitPoints - 1];
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        ApplyCollisionLogic(ball);
+    }
+
+    private void ApplyCollisionLogic(Ball ball)
+    {
+        this.HitPoints--;
+        if (this.HitPoints <= 0)
+        {
+            OnBrickDestruction?.Invoke(this);
+            SpawnDestroyEffect();
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            this.sr.sprite = BricksManager.Instance.Sprites[this.HitPoints - 1];
+            // Change the sprite
+        }
+    }
+
+    private void SpawnDestroyEffect()
+    {
+        Vector3 brickPos = gameObject.transform.position;
+        Vector3 spawnPos = new Vector3(brickPos.x, brickPos.y, brickPos.z - 0.2f);
+        GameObject effect = Instantiate(DestroyEffect.gameObject, spawnPos, Quaternion.identity);
+
+        MainModule mm = effect.GetComponent<ParticleSystem>().main;
+        mm.startColor = this.sr.color;
+        Destroy(effect, DestroyEffect.main.startLifetime.constant);
+    }
+}
---
+++ Assets/Scripts/BricksManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BricksManager : Singleton<BricksManager>
+{
+    public Sprite[] Sprites;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
